49a3f5c62260880c80296fa956f8e8e8de0bc3fb
malayverma78 <mv4024@srmist.edu.in>
2026-02-26 09:36:56 +0530

    modified commit by me

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,10 +1,18 @@
 public class PalindromeCheckerApp {
-    /*
-        UC1
-    */
+
     public static void main(String[] args) {
-        System.out.println("Welcome to the Palindrome Checker Management System\n");
-        System.out.println("Version : 1.0");
-        System.out.println("System initialized successfully");
+
+        String word = "madam";
+        String reversedWord = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversedWord = reversedWord + word.charAt(i);
+        }
+
+        if (word.equals(reversedWord)) {
+            System.out.println("The word \"" + word + "\" is a palindrome.");
+        } else {
+            System.out.println("The word \"" + word + "\" is not a palindrome.");
+        }
     }
 }
